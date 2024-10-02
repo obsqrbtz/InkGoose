aab984ce41698480a736e796d4cc3a4ccb8807b2
Daniel Dada <doesdeos@gmail.com>
2024-10-02 22:10:11 +0300

    send jsons for creating and updating notes, corrected modals

--- Frontend/InkGoose.Web/src/components/NoteCreate.vue
+++ Frontend/InkGoose.Web/src/components/NoteCreate.vue
@@ -1,21 +1,25 @@
 <template>
     <teleport to="body">
-        <div class="absolute sticky top-0 inset-0 overflow-y-auto bg-black bg-opacity-50" v-if="isOpen">
-            <div class="flex items-start justify-center h-full mt-24 text-center">
-                <div class="bg-white text-black rounded-lg shadow-xl w-6/12 m-4">
+        <div class="fixed inset-0 overflow-y-auto bg-black bg-opacity-50" v-if="isOpen">
+            <div class="flex items-start justify-center h-4/5 mt-24 text-center">
+                <div class="bg-white text-black rounded-lg shadow-xl w-6/12 h-full m-4">
                     <slot></slot>
-                    <div class="flex items-end w-full justify-end p-2">
-                        <button @click="close" class="btn btn-sm btn-circle btn-ghost">✕</button>
+                    <div class="flex w-full p-2">
+                        <div class="flex w-full justify-start">
+                            <button @click="createNote()" class="btn btn-sm btn-neutral">Save</button>
+                        </div>
+                        <div class="flex w-full justify-end">
+                            <button @click="close" class="btn btn-sm btn-circle btn-ghost">✕</button>
+                        </div>
                     </div>
                     <div class="p-2">
-                        <input @input="updateTitle($event.target.value)" type="text" placeholder="Title"
+                        <input @input="updateTitle($event.target.value)" type="text" :value="title" placeholder="Title"
                             class="input w-full focus:border-none focus:outline-none font-bold" />
-                        <textarea @input="updateContent($event.target.value)"
-                            class="textarea textarea-ghost textarea-lg whitespace-break-spaces w-full focus:border-none focus:outline-none"
-                            placeholder="Content"></textarea>
                     </div>
-                    <div class="flex items-center justify-end p-2">
-                        <button @click="createNote" class="btn btn-sm btn-neutral">Save</button>
+                    <div class="h-3/4">
+                        <textarea @input="updateContent($event.target.value)"
+                            class="textarea textarea-ghost textarea-lg h-full whitespace-break-spaces w-full focus:border-none focus:outline-none"
+                            :value="noteContent" placeholder="Content"></textarea>
                     </div>
                 </div>
             </div>
@@ -40,19 +44,19 @@ export default {
             this.$emit('update:isOpen', false);
         },
         async createNote() {
-            var requestParams = "";
-            if (editTitle != undefined) {
-                requestParams += `&title=${editTitle}`;
-            }
-            if (editContent != undefined) {
-                requestParams += `&content=${editContent}`;
-            }
-            const response = await fetch(`${this.apiHost}/Notes/AddNote?${requestParams}`, {
+            var params = {
+                id: this.id,
+                title: editTitle,
+                content: editContent
+            };
+            var reqBody = JSON.stringify(params);
+            const response = await fetch(`${this.apiHost}/Notes/AddNote`, {
                 method: "POST",
                 headers: {
                     "Content-type": "application/json; charset=UTF-8",
                     "Authorization": `Bearer ${window.localStorage.getItem("accessToken")}`
-                }
+                },
+                body: reqBody
             });
             if (response.status === 401) {
                 window.localStorage.removeItem("accessToken");
